78ab31033ddc3345112ddbb0da7bd64326f3b8a6
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-30 11:46:54 +0530

    All Get Routes Completed and tested

--- routes/1.)GetRoutes.go
+++ routes/1.)GetRoutes.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,7 +23,7 @@ func GetAllQuestions(c *fiber.Ctx) error {
 
 	cursor, err := collection.Find(c.Context(), bson.M{})
 	HandleError(err)
-	defer cursor.Close(c.Context())
+	defer cursor.Close(context.Background())
 
 	for cursor.Next(c.Context()) {
 		var qn Question
@@ -33,13 +36,38 @@ func GetAllQuestions(c *fiber.Ctx) error {
 }
 
 func GetQuestionById(c *fiber.Ctx) error {
-	// code for finding question by id
-	return nil
+	id := c.Params("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	HandleError(err)
+
+	filter := bson.M{"_id": objectId}
+	var qn Question
+
+	err = collection.FindOne(context.Background(), filter).Decode(&qn)
+	HandleError(err)
+
+	return c.Status(200).JSON(qn)
 }
 
 func GetQuestionByDifficulty(c *fiber.Ctx) error {
-	// code for finding question by difficulty
-	return nil
+	difficulty := c.Params("difficulty")
+	difficulty = strings.ToLower(difficulty)
+
+	filter := bson.M{"difficulty": difficulty}
+
+	var qns []Question
+	cursor, err := collection.Find(context.Background(), filter)
+	HandleError(err)
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var qn Question
+		err := cursor.Decode(&qn)
+		HandleError(err)
+		qns = append(qns, qn)
+	}
+
+	return c.Status(200).JSON(qns)
 }
 
 func HandleError(err error) {
